6ffb9f29333fcbb0040cdfbdccb411015f1cfa43
EarendilM83 <nukri.tusishvili@gmail.com>
2021-10-09 19:54:51 +0400

    fixed code alignments and styles

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
+let passwordField = document.getElementById("pass");
 
-    let passwordField = document.getElementById('pass');
+// get validations fields
+let emailValidation = document.getElementById("email-validations");
+let passwordValidation = document.getElementById("password-validations");
+let emailField = document.getElementById("emailField");
+let form = document.getElementById("Form");
+let emailPlaceholder = document.getElementById("emailPlaceholder");
+let passPlaceholder = document.getElementById("passPlaceholder");
+let submitbtn = document.getElementById("submitBtn");
+let pattern = /\S+@\S+\.\S+/;
 
-    // get validations fields
-    let emailValidation = document.getElementById('email-validations');
-    let passwordValidation = document.getElementById('password-validations');
-    let emailField = document.getElementById('emailField');
-    let form = document.getElementById('Form');
-    let emailPlaceholder = document.getElementById('emailPlaceholder');
-    let passPlaceholder = document.getElementById('passPlaceholder')
-    let submitbtn = document.getElementById('submitBtn')
-    let pattern = /\S+@\S+\.\S+/;
+// show/hide password
+let showHide = () => {
+  let show = document.getElementsByClassName("invisible")[0];
 
-    // show/hide password
-    let showHide = () => {
-        let show = document.getElementsByClassName('invisible')[0];
-        
-            show.classList.toggle('visible');
+  show.classList.toggle("visible");
 
-        if ( passwordField.type === 'password' ){
-
-            
-
-            passwordField.type = 'text';
-        }
-        else{
-            passwordField.type = "password"
-        }
-    }
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+  } else {
+    passwordField.type = "password";
+  }
+};
 
 // email and password validations
 // form.addEventListener('submit', (e) => {
 //     let emailErrors= [];
 //     let passwordErrors = [];
-    
 
 //     if(emailField.value === '' || emailField.value == null){
 //         emailErrors.push('Email field is required')
-//     }  
-    
-
-    
-  
+//     }
 
 //     if(passwordField.value === '' || passwordField.value == null){
 //         passwordErrors.push('Password is required')
@@ -57,37 +47,31 @@
 //     }
 // });
 
+submitbtn.addEventListener("click", () => {
+  if (emailField.value.match(pattern)) {
+    emailValidation.innerText = "Your email is valid";
+    emailField.style.borderColor = "#F5B118";
+  } else {
+    emailPlaceholder.style.color = "red";
+    emailField.style.borderColor = "red";
+    emailValidation.innerText = "Invalid Email Format";
 
-
-submitbtn.addEventListener('click', ()  => {
-    if(emailField.value.match(pattern)){
-        emailValidation.innerText = 'Your email is valid';
-        emailField.style.borderColor = '#F5B118'
-        
-     }
-   else{
-        emailPlaceholder.style.color = 'red';
-        emailField.style.borderColor = 'red';
-        emailValidation.innerText = 'Invalid Email Format';
-
-        if(passwordField.value === '' || passwordField.value == null){
-            passwordValidation.innerText = 'Password cannot be empty'
-        } else if(passwordField.value.length > 0 && passwordField.value.length <6){
-            passwordValidation.innerText = 'Password lenght is less than 6 characters'
-        }else{
-            return true;
-        }
-        
-
+    if (passwordField.value === "" || passwordField.value == null) {
+      passwordValidation.innerText = "Password cannot be empty";
+    } else if (
+      passwordField.value.length > 0 &&
+      passwordField.value.length < 6
+    ) {
+      passwordValidation.innerText =
+        "Password lenght is less than 6 characters";
+    } else {
+      return true;
     }
-   
-})
+  }
+});
 
-
-
-    console.log(emailValidation);
-    console.log(passwordValidation);
-    console.log(emailField);
-    console.log(pass);
-    console.log(form);
-
+console.log(emailValidation);
+console.log(passwordValidation);
+console.log(emailField);
+console.log(pass);
+console.log(form);
